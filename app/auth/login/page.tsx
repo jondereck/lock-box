40d116b04aa00855c33b6b2fd5c65ffb1d454923
jondereck <jonderecknifas@gmail.com>
2024-03-21 14:36:52 +0800

    fix

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,9 +1,13 @@
 import { LoginForm } from "@/components/auth/login-form";
+import { Suspense } from "react";
 
 const Login = () => {
-  return ( 
-    <LoginForm/>
-   );
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <LoginForm />
+    </Suspense>
+
+  );
 }
- 
+
 export default Login;
